238ac799e057da734edcf1fbf8a63c56cc96e746
Michael Dougherty <maackle.d@gmail.com>
2017-07-23 00:19:35 -0700

    Mark endpoint

--- server/app.js
+++ server/app.js
@@ -2,11 +2,25 @@ const express = require('express')
 const neo4j = require('neo4j-driver').v1
 const osmosis = require('osmosis')
 const request = require('request')
+const R = require('ramda')
 
 const app = express()
 
 const db = neo4j.driver('bolt://localhost', neo4j.auth.basic("neo4j", "neo4j"));
 
+const cypher = (query, params) => {
+  const session = db.session()
+  return session.run(query, params)
+    .then(res => {
+      session.close()
+      return res
+    })
+    .catch(err => {
+      console.error('QUERY ERROR: ', err)
+      session.close()
+      return err
+    })
+}
 
 const getLinks = (url, then) => {
   let links = [], done = false;
@@ -50,21 +64,40 @@ app.get('/lookup/:term', (req, res) => {
   const {term} = req.params
   const url = `https://en.wikipedia.org/wiki/${term}`
   getLinks(url, links => {
-    const session = db.session()
-    links.map(link => {
-      const {slug, name} = link
-      const query = `
-        MERGE (c:Article {slug: {term} })
-        MERGE (a:Article {slug: {slug} })
-        MERGE (c)-[:LINK {name: {name}}]->(a)
-      `
-      console.log('QQQQ', query)
-      session.run(query, {term, slug, name}).then(result => {
-        session.close()
-        console.log('REZZ', result)
-      }).catch((...args) => console.error("ERROR", args))
+    const query = `
+      MERGE (c:Article {slug: {term} })
+      FOREACH (
+        d IN {links} |
+        MERGE (a:Article {slug: d.slug})
+        MERGE (c)-[:LINK {name: d.name}]->(a)
+      )
+    `
+    cypher(query, {term, links}).then(() => {
+      res.send(JSON.stringify(links))
     })
-    res.send(JSON.stringify(links))
+  })
+})
+
+app.get('/mark/:term/:level', (req, res) => {
+  const userId = 11  // TODO
+  const {term} = req.params
+  const level = R.toLower(req.params.level)
+  if (!R.contains(level, ['yes', 'no'])) {
+    res.send("invalid relationship type")
+    return
+  }
+  const relationship = level == 'yes' ? ':KNOWN_YES' : ':KNOWN_NO'
+  const query = `
+    MATCH (a:Article {slug: {term} })
+    MATCH (u:User {id: {userId} })
+    OPTIONAL MATCH (u)-[r:KNOWN_YES|:KNOWN_NO]->(a)
+    DELETE r
+    MERGE (u)-[${relationship}]->(a)
+    RETURN u
+  `
+  cypher(query, {term, userId}).then(r => {
+    console.log(r)
+    res.send("OK")
   })
 })
 
